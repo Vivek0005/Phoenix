3a67c3e00f3cea0f37b52de06f1f00c7dc775815
Vivek <151066010+Vivek0005@users.noreply.github.com>
2025-05-31 01:22:40 +0530

    Update server.js to register middleware and route handlers

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const app = express();
 const gadgetsRoutes = require("./src/routes/gadgetR");
+const authRoutes = require("./src/routes/userR");
 
 //middlewares
 app.use(express.json());
 app.use("/gadgets", gadgetsRoutes);
+app.use("/auth", authRoutes);
 
 // home page
 app.get("/", (req, res) => {
